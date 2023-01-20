56917fd33c3d67a530d9fc4d3368f049427f8810
Enzo-Bonnin <91955208+enzo-bonnin@users.noreply.github.com>
2023-01-20 16:40:07 +0100

    les attributs de la recette sous forme de tableau

--- Vues/helloworld/voir.php
+++ Vues/helloworld/voir.php
@@ -1,16 +1,20 @@
-
-
-
-
 <?php
 $recette = ModeleRecette::laRecette();
+$i = 0;
+$arr[' '] = '';
 // Ajouter la requête à la variable $A_vue
 $A_vue['recette'] = $recette;
 // Supprimer les doublons
 $A_vue['recette'] = array_unique($A_vue['recette']);
+// Récupérer les clés
+$clés = array_keys($A_vue['recette']);
+// Créer un tableau
+echo "<table>";
 // Parcourir le tableau et récupérer les valeurs
 foreach ($A_vue['recette'] as $value) {
-    // Afficher la vue
-    echo "<p>" . $value . "</p>";
+    // Afficher le nom de l'attribut et la valeur dans une ligne de tableau
+    echo "<tr><td>" . $clés[$i] . "</td><td>" . $value . "</td></tr>";
+    $i++;
 }
+echo "</table>";
 ?>
