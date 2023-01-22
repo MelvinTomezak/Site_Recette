9737513c07e681d66d507e3cd62c87a6ccdbf71e
Djibril Ramdani <92161799+DjibrilRamdani@users.noreply.github.com>
2023-01-22 21:29:14 +0100

    Add files via upload
    J'ai fait feur ( pied de page css)

---
+++ pied.php
@@ -0,0 +1,31 @@
+<footer>
+    <div>
+        <p>&copy; 2023 Site Recette. Tous droits réservés.</p>
+        <p>Suivez-nous sur : <a href="#">Facebook</a> <a href="#">Twitter</a> <a href="#">Instagram</a></p>
+        <p>Contactez-nous : <a href="mailto:contact@siterecette.com">contact@siterecette.com</a> | +33 (0)1 23 45 67 89</p>
+        <p><a href="#">Politique de confidentialité</a> | <a href="#">Cookies</a></p>
+    </div>
+    <div>
+        <p><a href="#">Accueil</a> | <a href="#">À propos de nous</a> | <a href="#">Recettes</a></p>
+        <p>Liens externes : <a href="#">Association de cuisine</a> | <a href="#">Site de recettes</a></p>
+    </div>
+</footer>
+
+<style>
+footer {
+background-color: #333333; /* couleur de fond */
+color: #ffffff; /* couleur du texte */
+padding: 20px; /* espace autour du contenu */
+display: flex; /* pour disposer les deux div côte à côte */
+justify-content: space-between; /* pour espacer les deux div */
+}
+
+footer a {
+color: #ffffff; /* couleur des liens */
+text-decoration: none; /* pour enlever le soulignement des liens */
+}
+
+footer a:hover {
+color: #cccccc; /* couleur des liens au survol */
+}
+</style>
