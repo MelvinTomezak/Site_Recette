f581dc3128af1f04f64370d3516e879fe493b68a
kaddouri <mohammed.kaddouri@etu.univ-amu.fr>
2023-01-22 17:40:49 +0100

    Version de Djibril

--- Vues/standard/entete.php
+++ Vues/standard/entete.php
@@ -1,49 +1,98 @@
-<?php
-echo '<ul class="navbar">';
-echo '<li><a href="home.php">Accueil</a></li>';
-echo '<li><a href="about.php">A propos</a></li>';
-echo '<li><a href="contact.php">Contact</a></li>';
-echo '<li class="right"><a href="login.php">Connexion</a></li>';
-echo '<li class="right"><a href="search.php">Rechercher</a></li>';
-echo '</ul>';
+<nav>
+    <ul class="navbar">
+        <li><a href="home.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li class="search-bar">
+            <form>
+                <input type="search" placeholder="Search...">
+                <button type="submit">Search</button>
+            </form>
+        </li>
+        <li class="right">
+            <?php if (isset($_SESSION['user'])) { ?>
+                <a href="profile.php">Profile</a>
+                <a href="logout.php">Logout</a>
+            <?php } else { ?>
+                <a href="login.php">Login</a>
+                <a href="register.php">Register</a>
+            <?php } ?>
+        </li>
+    </ul>
+</nav>
 
-?>
+
 
 <head>
     <style>
-        ul.navbar {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-           // background-color: white;
+        nav {
             background: linear-gradient(#e66465, #9198e5);
             border-radius: 10px;
             border: 2px solid #2a5c8e;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 10px;
         }
 
-        ul.navbar li {
-            float: left;
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
         }
 
-        ul.navbar li a {
-            display: block;
+        nav ul li {
+            margin: 0 10px;
+        }
+
+        nav ul li a {
             color: black;
-            text-align: center;
-            padding: 14px 16px;
             text-decoration: none;
             font-family: sans-serif;
             font-weight: bold;
         }
 
-        ul.navbar li a:hover {
+        nav ul li a:hover {
             background-color: #2f6cc9;
+            color: white;
+            padding: 10px;
+            border-radius: 10px;
         }
 
-        ul.navbar li.right {
-            float: right;
+        nav ul li.search-bar {
+            margin-left: auto;
+            margin-right: auto;
         }
+
+        nav form {
+            display: flex;
+            align-items: center;
+        }
+
+        nav input[type="search"] {
+            padding: 10px;
+            border: none;
+            border-radius: 10px;
+            margin-right: 10px;
+        }
+
+        nav button[type="submit"] {
+            background-color: #2f6cc9;
+            color: white;
+            border: none;
+            border-radius: 10px;
+            padding: 10px 20px;
+            font-weight: bold;
+            cursor: pointer;
+        }
+
+        nav ul li.right {
+            margin-left: auto;
+        }
+
+
     </style>
 </head>
-
-
