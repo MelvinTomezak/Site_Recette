ac3bd187d32827c3fb632eaf9d6c509fbf9d4a70
MohammedKaddouri <98416541+MohammedKaddouri@users.noreply.github.com>
2023-01-22 18:24:51 +0100

    Delete navbar.php

--- Vues/standard/navbar.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo '<ul class="navbar">';
-echo '<li><a href="home.php">Accueil</a></li>';
-echo '<li><a href="about.php">A propos</a></li>';
-echo '<li><a href="contact.php">Contact</a></li>';
-echo '</ul>';
-?>
