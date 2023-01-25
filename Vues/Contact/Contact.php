4d451b435e46e8951df30613ea64171b9a04c5ff
Djibril <djibril.ramdani@etu.univ-amu.fr>
2023-01-25 12:22:45 +0100

    Page contact

--- Vues/Contact/Contact.php
+++ Vues/Contact/Contact.php
@@ -1,53 +1,102 @@
-<link rel="stylesheet" type="text/css" href="style.css">
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        body{
-            background: linear-gradient(#e66465, #9198e5);        }
-        .rectangle {
-            width: 300px;
-            height: 300px;
-            background: red;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin: 10px;
-            padding: 10px;
-            border: 1px solid #000;
-            float: :left;
-            border-radius: 10px;
-            box-shadow: 1px 1px 20px black;
-
-        }
-    </style>
+    <link rel="stylesheet" href="contact.css" />
+    <script type="text/javascript" src="contact.js"></script>
 </head>
-<body >
-
-<div style="display: flex">
-    <?php
-    $i = 0;
-    $recette = ModeleRecette::laRecette();
-    $arr[' '] = '';
-    // Ajouter la requête à la variable $A_vue
-    $A_vue['recette'] = $recette;
-    // Supprimer les doublons
-    $A_vue['recette'] = array_unique($A_vue['recette']);
-    $i = 0;
-    foreach ($A_vue['recette'] as $value) {
-        echo "<div class='rectangle'>";
-        echo "<table>";
-        foreach ($A_vue['recette'] as $key => $value) {
-            echo "<tr><td>" . $key . "</td><td>" . $value . "</td></tr>";
-        }
-        echo "</table>";
-        echo "</div>";
-        $i++;
-    }
-    ?>
+<body>
+<div id="box">
+    <form id="form" enctype="multipart/form-data" onsubmit="return validate()" method="post">
+        <h3>Formulaire de contact</h3>
+        <label>Nom: <span>*</span></label>
+        <input type="text" id="name" name="name" placeholder="Nom"/>
+        <label>Email: <span>*</span></label><span id="info" class="info"></span>
+        <input type="email" id="email" name="email" placeholder="Email"/>
+        <label>Sujet: <span>*</span></label>
+        <input type="text" id="subject" name="subject" placeholder="Demande de renseignement"/>
+        <label>Message:</label>
+        <textarea id="message" name="message" placeholder="Message..."></textarea>
+        <input type="submit" name="send" value="Envoyer le message"/>
+    </form>
 </div>
 </body>
 </html>
+
+
+<style>
+
+    body
+    {
+        background: linear-gradient(#e66465, #9198e5); ;
+    }
+    #box {
+        width: 955px;
+        margin: 20px auto;
+        padding-top: 20px;
+        font-family: serif;
+        text-align: center;
+    }
+    #form {
+        margin: 20px auto 0 auto;
+        border-radius: 2px;
+        padding: 20px 30px;
+        box-shadow: 0 0 15px;
+        font-size: 14px;
+        font-weight: bold;
+        width: 350px;
+        float: none;
+    }
+    h3{
+        text-align:center;
+        font-size:20px;
+    }
+    input{
+        width:100%;
+        height:35px;
+        margin-top:5px;
+        margin-bottom: 20px;
+        border:1px solid #999;
+        border-radius:3px;
+        padding:5px;
+    }
+    input[type=submit]{
+        background:linear-gradient(#e66465, #9198e5); ;
+        border:1px solid white;
+        font-family: serif;
+        font-Weight:bold;
+        font-size:18px;
+        color:white;
+    }
+    textarea{
+        width:100%;
+        height:80px;
+        margin-top:5px;
+        border-radius:3px;
+        padding:5px;
+        resize:none;
+    }
+    span{
+        color:red
+    }
+    .successMessage{
+        background-color: #7acc7d;
+        border: #2b5a2d 1px solid;
+        padding: 5px 9px;
+        color: #262b26;
+        border-radius: 4px;
+        cursor: pointer;
+        font-size: 0.9em;
+    }
+    .errorMessage{
+        background-color: #e64141;
+        border: #da1414 1px solid;
+        padding: 5px 10px;
+        color: #fdf7f7;
+        border-radius: 4px;
+    }
+    .info{
+        font-size: 0.9em;
+        color: #d67262;
+        letter-spacing: 2px;
+        padding-left: 5px;
+    }
+</style>
