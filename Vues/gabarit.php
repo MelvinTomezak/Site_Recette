f3c0aa23f4c12de85a9393a6a4ee97f140a4e939
Enzo-Bonnin <91955208+enzo-bonnin@users.noreply.github.com>
2023-01-25 16:29:27 +0100

    cahngement de bug sur le gabarit

--- Vues/gabarit.php
+++ Vues/gabarit.php
@@ -8,9 +8,7 @@
     </head>
     <body>
         <?php Vue::montrer('standard/entete');?>
-
-        <?php Vue::montrer('Accueil/Accueil');?>
-
+        <?php echo $A_vue['body'] ?>
         <?php Vue::montrer('standard/pied');?>
 
     </body>
