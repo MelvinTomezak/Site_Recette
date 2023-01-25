e5bf4798909ea5ca71f790e5a61509bf84d1ef9c
Enzo-Bonnin <91955208+enzo-bonnin@users.noreply.github.com>
2023-01-26 00:33:46 +0100

    boutton pour les 3 recette de l'accueil

--- Vues/Accueil/Accueil.php
+++ Vues/Accueil/Accueil.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="fr">
 
@@ -28,21 +29,37 @@
             padding: 10px;
 
         }
+
+        .title {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        .text {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
     </style>
 </head>
 <div  class='box-recette'">
 <?php
 
 for ($i = 0; $i < 3; $i++) {
-?>
-<div class='body'>
-    <h2 class="title"><?php echo $A_vue['recette'][$i]['photographie'] ?></h2>
-    <p class="text">Difficulté :<?php echo$A_vue['recette'][$i]['difficulte']?></p>
-    <p class="text">temps de preparation :  <?php echo$A_vue['recette'][$i]['temps_preparation'] ?> min</p>
-    <p class="text">note_moyenne :  <?php echo$A_vue['recette'][$i]['note_moyenne'] ?>/20</p>
-    <a href="Connexion/Login.php"><button><?= $A_vue['recette'][$i]['id'] ?> Recette</button></a>
-</div>
+    ?>
+    <div class="body" >
+        <h2 class="title"><?php echo $A_vue['recette'][$i]['photographie'] ?></h2>
+        <p class="text">Difficulté :<?php echo$A_vue['recette'][$i]['difficulte']?></p>
+        <p class="text">temps de preparation :  <?php echo$A_vue['recette'][$i]['temps_preparation'] ?> min</p>
+        <p class="text">note_moyenne :  <?php echo$A_vue['recette'][$i]['note_moyenne'] ?>/20</p>
+        <a href="/Recette/recette/<?php echo $A_vue['recette'][$i]['id'] ?>"><button><?= $A_vue['recette'][$i]['id'] ?> Recette</button></a></div>
 
-<?php
+    <?php
 }
 ?>
+</div>
+<div  class='box-recette'">
+
+<a1 href="/Recette/recette/"><button>Catalogue de recette</button></a1></div>
+</html>
