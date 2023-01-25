bc06e8b4c5c1b13be0ed370ae5dff63c8d1323b8
Djibril <djibril.ramdani@etu.univ-amu.fr>
2023-01-25 19:02:24 +0100

    ajout Page a propos de nous

---
+++ Site_Recette-main/Vues/About/aPropos.php
@@ -0,0 +1,30 @@
+<?php include '../standard/entete.php'; ?>
+<!doctype html>
+<html lang="fr">
+
+<link rel="stylesheet" href="about.css">
+
+<h1>À propos de nous</h1>
+
+<p>Bienvenue sur notre site de recettes ! Nous sommes un groupe d'amis passionnés de cuisine qui aimons partager nos recettes favorites avec le monde. Nous croyons que la cuisine est un moyen formidable de rassembler les gens et de célébrer la diversité culturelle.</p>
+
+<h2>Notre équipe</h2>
+
+<ul>
+    <li>Djibril - Chef de cuisine professionnelle et auteur de livres de cuisine</li>
+    <li>Mohammed - Expert en gateaux arabes</li>
+    <li>Melvin - Cuisinier amateur et spécialiste des recettes végétariennes</li>
+    <li>Enzo C. - Expert en cuisine internationale et amoureus de la découverte de nouveaux ingrédients</li>
+    <li>Enzo B. - Amoureux de la gastronomie française et expert en vins</li>
+    <li>Alex - Amoureux de la gastronomie brésilienne </li>
+    <li>Amaury - Ne sert pas à grand chose</li>
+
+</ul>
+
+<h2>Pourquoi nous aimons partager nos recettes ?</h2>
+
+<p>Nous aimons partager nos recettes car nous croyons que la cuisine est un moyen de rassembler les gens et de célébrer la diversité culturelle. Nous aimons également aider les gens à découvrir de nouveaux ingrédients et techniques culinaires, et à améliorer leurs compétences en cuisine. En partageant nos recettes, nous espérons inspirer les gens à explorer de nouveaux horizons culinaires et à apprécier la nourriture de manière plus consciente et plus savoureuse.</p>
+
+<h2>Contactez-nous</h2>
+
+<p>Si vous avez des questions ou des commentaires à propos de notre site ou de nos recettes, n'hésitez pas à nous contacter à l'adresse suivante : <a href="mailto:contact@notresite.com">contact@notresite.com</a>.</p>
