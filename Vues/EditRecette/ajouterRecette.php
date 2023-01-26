d1568c7bd514212aabb849d5a45d50b6345a2cc6
CANALEenzo <enzo.canale@etu.univ-amu.fr>
2023-01-26 17:52:33 +0100

    placement des boutons radios sous les textes

--- Vues/EditRecette/ajouterRecette.php
+++ Vues/EditRecette/ajouterRecette.php
@@ -37,38 +37,47 @@
         <label for="temps_preparation">Temps de préparation :</label>
         <input type="number" id="temps_preparation" name="temps_preparation" class="form-control">
     </div>
+
+
     <div class="form-group">
         <label for="difficulte" id="difficulte">Difficulté :</label>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="difficulte" value="très facile">
             <label class="form-check-label" for="tres_facile">Très facile</label>
+            <input class="form-check-input" type="radio" name="difficulte" value="très facile">
+
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="difficulte" value="facile">
             <label class="form-check-label" for="facile">Facile</label>
+            <input class="form-check-input" type="radio" name="difficulte" value="facile">
+
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="difficulte" value="moyen">
             <label class="form-check-label" for="moyen">Moyen</label>
+            <input class="form-check-input" type="radio" name="difficulte" value="moyen">
+
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="difficulte" value="difficile">
             <label class="form-check-label" for="difficile">Difficile</label>
+            <input class="form-check-input" type="radio" name="difficulte" value="difficile">
+
         </div>
     </div>
     <div class="form-group">
         <label for="cout" id="cout">Cout :</label>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="cout" value="bon marche">
             <label class="form-check-label" for="bon marche">bon marche</label>
+            <input class="form-check-input" type="radio" name="cout" value="bon marche">
+
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="cout" value="cout moyen">
             <label class="form-check-label" for="cout moyen">cout moyen</label>
+            <input class="form-check-input" type="radio" name="cout" value="cout moyen">
+
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="cout" value="assez cher">
             <label class="form-check-label" for="assez cher">assez cher</label>
+            <input class="form-check-input" type="radio" name="cout" value="assez cher">
+
         </div>
     </div>
     <div class="form-group">
@@ -82,20 +91,24 @@
     <div class="form-group">
         <label for="liste_particularite" id="liste_particularite">Particularite :</label>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="liste_particularite" value="vegetarien">
             <label class="form-check-label" for="vegetarien">vegetarien</label>
+            <input class="form-check-input" type="radio" name="liste_particularite" value="vegetarien">
+
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="liste_particularite" value="vegan">
             <label class="form-check-label" for="vegan">vegan</label>
+            <input class="form-check-input" type="radio" name="liste_particularite" value="vegan">
+
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="liste_particularite" value="sans gluten">
             <label class="form-check-label" for="sans gluten">sans gluten</label>
+            <input class="form-check-input" type="radio" name="liste_particularite" value="sans gluten">
+
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="liste_particularite" value="sans lactose">
             <label class="form-check-label" for="sans lactose">sans lactose</label>
+            <input class="form-check-input" type="radio" name="liste_particularite" value="sans lactose">
+
         </div>
     </div>
     <button type="submit" class="btn btn-primary">Ajouter une recette</button>
