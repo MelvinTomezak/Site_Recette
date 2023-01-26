492852a2fed5ff91db01410f7eac3d006f49f0bb
CANALEenzo <enzo.canale@etu.univ-amu.fr>
2023-01-26 23:36:01 +0100

    commentaire des fonctions

--- Modele/Categorie.php
+++ Modele/Categorie.php
@@ -2,13 +2,21 @@
 
 class Categorie
 {
+    // Méthode pour rechercher une recette en fonction de sa difficulté
     public function rechercherRecette($recherche){
+        // Connexion à la base de données en utilisant les informations de connexion données
         $db = Database::connect("rogue.db.elephantsql.com","ykutlvtz","3bqbVY-4n626jHaAdvIIraI3Ds5QcD4N");
+        // Requête SQL pour sélectionner toutes les recettes où la colonne "difficulté" contient la chaîne de recherche
         $req = 'SELECT * FROM recette WHERE difficulte LIKE :recherche';
+        // Préparation de la requête
         $req_prep = $db->prepare($req);
+
         $req_prep->bindValue(':recherche', "%$recherche%", PDO::PARAM_STR);
+        // Exécution de la requête
         $req_prep->execute();
+        // Stockage des résultats de la requête dans la variable $resultat
         $resultat = $req_prep->fetchAll();
+        // Retour des résultats de la requête
         return $resultat;
     }
 }
--- Modele/ModeleAppreciation.php
+++ Modele/ModeleAppreciation.php
@@ -1,34 +1,59 @@
 <?php
 require_once("../Noyau/Database/Database.php");
 
-class ModeleAppreciation{
+class ModeleAppreciation
+{
 
-    public static function readAll(){
+    // Méthode pour lire toutes les lignes de la table "appreciation"
+    public static function readAll()
+    {
+        // Requête SQL pour sélectionner toutes les lignes de la table "appreciation"
         $sql = "SELECT * FROM appreciation";
+        // Exécution de la requête et stockage des résultats dans la variable $rep
         $rep = Database::$con->query($sql);
+        // Définition du mode de fetch pour que chaque ligne soit retournée sous la forme d'un objet de la classe "appreciation"
         $rep->setFetchMode(PDO::FETCH_CLASS, 'appreciation');
+        // Retour des résultats de la requête
         return $rep->fetchAll();
     }
 
-    public function create(){
+    // Méthode pour créer une nouvelle ligne dans la table "appreciation"
+    public function create()
+    {
+        // Requête SQL pour insérer une nouvelle ligne dans la table "appreciation"
         $sql = "INSERT INTO appreciation(id) VALUES (:idT)";
+        // Préparation de la requête
         $req_prep = Database::$con->prepare($sql);
+        // Définition des valeurs à insérer dans la requête
         $values = array("id" => NULL);
+        // Exécution de la requête
         $req_prep->execute($values);
     }
 
-    public function update(){
+    // Méthode pour mettre à jour une ligne existante dans la table "appreciation"
+    public function update()
+    {
+        // Requête SQL pour mettre à jour la ligne correspondant à l'objet courant dans la table "appreciation"
         $sql = 'UPDATE appreciation SET id = :idT';
+        // Préparation de la requête
         $req_prep = Database::$con->prepare($sql);
+        // Définition des valeurs à mettre à jour dans la requête
         $values = array("appreciation" => $this->id);
+        // Exécution de la requête
         $req_prep->execute($values);
     }
 
-    public function delete(){
+
+// Méthode pour supprimer une ligne de la table "appreciation"
+    public function delete()
+    {
+        // Requête SQL pour supprimer la ligne correspondant à l'objet courant de la table "appreciation"
         $sql = "DELETE FROM appreciation WHERE id = :idT";
+        // Préparation de la requête
         $req_prep = Database::$con->prepare($sql);
+        // Définition des valeurs à utiliser pour la suppression dans la requête
         $values = array("id" => $this->id);
+        // Exécution de la requête
         $req_prep->execute($values);
     }
-
 }
